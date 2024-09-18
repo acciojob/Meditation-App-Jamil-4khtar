626544add69e079b727b225de2022957f738dc59
Jamil-4khtar <jamil4khtar@gmail.com>
2024-09-18 08:36:21 +0530

    update basic functions

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
-let app = document.querySelector(".app");
-let vidCont = document.querySelector(".video-container")
+let app = document.querySelector("#app");
+let vidCont = document.querySelector(".vid-container")
 let AudioCont = document.querySelector(".player-container")
 let pickSound = document.querySelector(".sound-picker")
 let timeDuration = document.querySelector("#time-select")
@@ -30,7 +30,7 @@ beachAudio.src = "./sounds/beach.mp3"
 rainAudio.src = "./sounds/rain.mp3"
 
 let td = document.createElement("h3"); td.className = "time-display";
-td.innerHTML = "10:0";
+td.innerHTML = "10:00";
 app.append(td)
 
 let pp = document.createElement("button"); pp.className = "play";
@@ -50,7 +50,7 @@ rainBtn.addEventListener("click", (e) => {
 
 })
 
-function switchMedia(video, audio) {
+function switchMedia(vid, aud) {
     if (currentVid && currentAudio) {
         currentVid.remove()
         currentAudio.remove()
@@ -58,15 +58,15 @@ function switchMedia(video, audio) {
         currentAudio.pause()
         
     }
-    vidCont.append(video)
-    AudioCont.append(audio)
-    currentVid = video;
-    currentAudio = audio;
+    vidCont.append(vid)
+    AudioCont.append(aud)
+    currentVid = vid;
+    currentAudio = aud;
 
     playDuration()
 }
 
-function playDuration(video, audio) {
+function playDuration(vid, aud) {
     timeDuration.innerHTML = ""// clear previous buttons
 
     let smaller = document.createElement("button"); smaller.id = "smaller-mins"; 
